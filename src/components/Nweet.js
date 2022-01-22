ff8a0338aa86561b51084e1755de76320ca616d7
김다슬 <kds@gimdaseul-ui-MacBookAir.local>
2022-01-22 16:25:34 +0900

    트윗 수정삭제 완료

---
+++ src/components/Nweet.js
@@ -0,0 +1,61 @@
+import { dbService } from "fbase";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore"
+import { useState } from "react";
+
+const Nweet = ({ nweetObj, isOwner}) => {
+
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const onDeleteClick = async() => {
+        const ok = window.confirm("삭제하시겠습니까?");
+        console.log(ok);
+        if (ok) {
+            console.log(nweetObj.id);
+            const data = deleteDoc(doc(dbService, "nweets", nweetObj.id));
+            console.log(data);
+        }
+    }
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+    
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewNweet(value);
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        //console.log(nweetObj.id, newNweet);
+        updateDoc(doc(dbService, "nweets", nweetObj.id), { text: newNweet });
+        setEditing(false);
+    };
+
+    return (
+        <div>
+            {editing? (
+                <>
+                <form onSubmit={onSubmit}>
+                    <input onChange={onChange} value={newNweet} required />
+                    <input type="submit" value="Update Nweet" />
+                </form>
+                <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                <h4>{nweetObj.text}</h4>
+                {isOwner && (
+                    <>
+                    <button onClick={onDeleteClick}>Delete Nweet</button>
+                    <button onClick={toggleEditing}>Edit Nweet</button>
+                    </>
+                )}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,34 +1,47 @@
 import { dbService  } from "fbase";
 import { useEffect, useState } from "react";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import Nweet from "components/Nweet";
 
-const Home = () => {
+const Home = ({userObj}) => {
+
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
 
-    const getNweets = async() => {
-        const dbNweets = await getDocs(collection(dbService,"nweets"));
+    // const getNweets = async() => {
+    //     const dbNweets = await getDocs(collection(dbService,"nweets"));
         
-        //dbNweets.forEach((document) => console.log(document.data()));
+    //     //dbNweets.forEach((document) => console.log(document.data()));
         
-        dbNweets.forEach((document) => {
-            const nweetObject = { ...document.data(), id: document.id};
-            setNweets((prev) => [nweetObject, ...prev])
+    //     dbNweets.forEach((document) => {
+    //         const nweetObject = { ...document.data(), id: document.id};
+    //         setNweets((prev) => [nweetObject, ...prev])
         
-        });
-    };
+    //     });
+    // };
 
     useEffect(() => {
-        getNweets();
+        //getNweets();
+        const q = query(collection(dbService, 'nweets'), orderBy('createdAt', 'desc'));
+        onSnapshot(q, (querySnapshot) => {
+            const newArray = querySnapshot.docs.map((doc) => {
+            return {
+                id: doc.id,
+                ...doc.data(),
+                }
+            });
+            setNweets(newArray);
+        });
+
     },[]);
 
-    console.log(nweets);
 
     const onSubmit = async (event) => {
         event.preventDefault();
         await addDoc(collection(dbService, "nweets"), {
             text: nweet,
             createdAt: Date.now(),
+            creatorId: userObj.uid
           });
         setNweet("");
     };
@@ -49,9 +62,10 @@ const Home = () => {
         </form>
         <div>
             {nweets.map((nweet) => (
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4> 
-                </div>
+                // <div key={nweet.id}>
+                //     <h4>{nweet.text}</h4> 
+                // </div>
+                <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
             ))}
         </div>
         </>
